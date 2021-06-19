9079329b5418b9b715921aefc4ec923aaf24fcba
Navy <navydotgif@gmail.com>
2021-06-19 22:33:16 +0300

    bruh

--- structure/Client.js
+++ structure/Client.js
@@ -95,7 +95,7 @@ class ModmailClient extends Client {
 
         const { prefix } = this;
         const { channel, guild, content, member } = message;
-        if (![this.mainServer.idm, this.bansServer.id].includes(guild.id)) return;
+        if (![this.mainServer.idm, this.bansServer?.id || '0'].includes(guild.id)) return;
         if (!content || !content.startsWith(prefix)) return;
 
         const roles = member.roles.cache.map((r) => r.id);
