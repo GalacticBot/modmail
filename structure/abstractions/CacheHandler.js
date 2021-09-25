1d09d3291b015d63aa5f7c044a6dd3a9685cbd90
Navy.gif <navydotgif@gmail.com>
2021-09-26 01:24:32 +0300

    begin work on some abstractions

---
+++ structure/abstractions/CacheHandler.js
@@ -0,0 +1,36 @@
+class CacheHandler {
+
+    constructor (client) {
+
+        this.client = client;
+
+    }
+
+    load () {
+        throw new Error('Not implemented');
+    }
+
+    savePersistentCache () {
+        throw new Error('Not implemented');
+    }
+
+    saveModmailHistory () {
+        throw new Error('Not implemented');
+    }
+
+    loadModmailHistory () {
+        throw new Error('Not implemented');
+    }
+
+    get json () {
+        return {
+            queue: this.queue,
+            channels: this.channels,
+            lastActivity: this.lastActivity,
+            misc: this.misc
+        };
+    }
+
+}
+
+module.exports = CacheHandler;
